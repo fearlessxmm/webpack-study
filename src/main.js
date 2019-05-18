5f85149d6c62f5bec539dab258520b504666d9f3
xmm <1210875741@qq.com>
2019-05-18 13:55:56 +0800

    feat:webpack认识与初步使用

---
+++ src/main.js
@@ -0,0 +1,11 @@
+// 项目的入口文件
+// es6代码一般主流浏览器解析不了
+// import *** from *** ES6导入包
+import $ from 'jquery';
+// const $ = require("jquery");相当于
+$(function(){
+    $('#list li:odd').css('backgroundColor','red');
+    $('#list li:even').css('backgroundColor',function(){
+        return '#' + 'ccc';
+    });
+})
