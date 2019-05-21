7bda3204f69e26a769b71ebf8eceb669a79effb1
minmin.xu <minmin.xu@mobvista.com>
2019-05-21 18:50:28 +0800

    feat:webpack各种loader模块

--- src/main.js
+++ src/main.js
@@ -3,6 +3,8 @@
 // import *** from *** ES6导入包
 import $ from 'jquery';
 // const $ = require("jquery");相当于
+import './css/style.css';
+import './css/style2.less';// 注意这里的less文件会被编译成css文件，如果与css同名，就会覆盖style.css文件
 $(function(){
     $('#list li:odd').css('backgroundColor','yellow');
     $('#list li:even').css('backgroundColor',function(){
--- webpack.config.js
+++ webpack.config.js
@@ -15,12 +15,19 @@ module.exports = {
         historyApiFallback: true,//不跳转
         inline: true//实时刷新
     },
-    plugins:[ // 添加plugins节点配置插件
+    plugins: [ // 添加plugins节点配置插件
         new htmlWebpackPlugin({
-            template:path.join(__dirname, 'src/index.html'),//模板路径
-            filename:'index.html'//自动生成的HTML文件的名称
+            template: path.join(__dirname, 'src/index.html'),//模板路径
+            filename: 'index.html'//自动生成的HTML文件的名称
         })
-    ]
+    ],
+    module: { // 用来配置第三方loader模块的
+        rules: [ // 文件的匹配规则
+            { test: /\.css$/, use: ['style-loader', 'css-loader'] },//处理css文件的规则
+            { test: /\.less$/, use: ['style-loader', 'css-loader', 'less-loader'] },// 处理less文件的规则
+            { test: /\.(png|jpg|gif)$/, use: 'url-loader?limit=1024&name=[name].[ext]' }
+        ]
+    }
 
     //使用--hot总是报错npm run dev2
     // devServer: {// 配置webpack-dev-server的第二种形式，比较麻烦
